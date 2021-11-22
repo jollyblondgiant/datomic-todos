114b555c5c0a5348e048a60096b357d3b2c022b9
Andy <andrew.carlil3@gmail.com>
2021-11-22 14:12:09 -0500

    normalize and improve api documentation

--- src/datomic_todos/routes.clj
+++ src/datomic_todos/routes.clj
@@ -10,19 +10,23 @@
     if using curl, be sure to use cookie flags, eg\n
     curl -b <path/to/cookies.txt> -c <path/to/cookies.txt> <server>:3000<request>\n"))
 
+(def links-guide
+  "links: :get-todos gets incomplete todos for logged in user;  :get-dones gets completed todos for logged in user; :task-chart returns a vector of task-counts by timestamp; :task-graph returns a visualization of the burndown data;  :logout ends session. :add-todo bears a %s token for interpolation of task description")
+
 (defn login
   "validates email
   and adds username cookie
-  then redirects home"
+  welcome to todo!"
   [{route :uri params :params
     :as request}]
   (if-let [user  (->> "user"
                       (get params)
                       (re-matches #"[a-zA-z0-9+_.-]+@[a-zA-Z0-9]+.[a-z]+"))]
-    (-> {:data "successfully logged in. refer to :meta > :links"
-         :meta {:docstring "a successful login attempt. links: :get-todos gets incomplete todos for logged in user;  :get-dones gets completed todos for logged in user; :task-chart returns a vector of task-counts by timestamp; :task-graph returns a visualization of the burndown data;  :logout ends session. "
+    (-> {:data "successfully logged in. refer to :meta"
+         :meta {:docstring links-guide
                 :links {:get-todos "/todos"
                         :get-dones "/dones"
+                        :add-todo "/add-todo?todo=%s"
                         :task-chart "/chart"
                         :task-graph "/graph"
                         :logout "/logout"
@@ -49,8 +53,8 @@
   [{route :uri params :params
     :as request}]
   (if-let [user (-> request :cookies (get "user") :value)]
-    (-> {:data "successfully logged in. refer to :meta > :links"
-         :meta {:docstring "a successful login attempt. links: :get-todos gets incomplete todos for logged in user;  :get-dones gets completed todos for logged in user; :task-chart returns a vector of task-counts by timestamp; :task-graph returns a visualization of the burndown data;  :logout ends session. "
+    (-> {:data (str "logged in as: " user)
+         :meta {:docstring links-guide
                 :links {:get-todos "/todos"
                         :get-dones "/dones"
                         :task-chart "/chart"
@@ -61,12 +65,6 @@
         ring.util.response/response)
     login-prompt))
 
-(def nav
-  '("<a href='/logout'>log out</a>" "|"
-    "<a href='/'>home</a>"          "<br/>"
-    "<span>add new todo by navigating to "
-    "localhost:3000/add-todo?todo={description}</span>")
-  )
 
 (defn dones
   "gets completed tasks
@@ -78,14 +76,15 @@
     (let [todos (todos/get-dones-by-user user)
           todo (fn [coll [label id]]
                  (conj coll {:data label
-                             :meta {:docstring ""
+                             :meta {:docstring "task description. task id formatted into :links > :incomplete-todo, which asserts task is incomplete and :links > :forget-todo, which retracts task."
                                     :links {:incomplete-todo (str "/incomplete-todo?id=" id)
                                             :forget-todo (str "/forget-todo?id=" id)}}}))]
       (-> (ring.util.response/response
            (->> todos (reduce todo [])
-                (assoc {:meta {:docstring ""
+                (assoc {:meta {:docstring "a vector of maps with :data = task description."
                                :links {:logout "/logout"
-                                       :home "/"}}} :data)
+                                       :add-todo "/add-todo?todo=%s"
+                                       :help "/"}}} :data)
                 generate-string))
           (ring.util.response/content-type "text/json")))
     login-prompt))
@@ -101,14 +100,14 @@
     (let [todos (todos/get-todos-by-user user)
           todo (fn [coll [label id]]
                  (conj coll {:data label
-                             :meta {:docstring ""
+                             :meta {:docstring "task description. task id formatted into :links > :complete-todo, which asserts task is complete and :links > :forget-todo, which retracts task."
                                     :links {:complete-todo (str "/complete-todo?id=" id)
                                             :forget-todo (str "/forget-todo?id=" id)}}}))]
       (-> (ring.util.response/response
            (->> todos (reduce todo [])
-                (assoc {:meta {:docstring ""
+                (assoc {:meta {:docstring "a list of maps with :data = task description."
                                :links {:logout "/logout"
-                                       :home "/"}}} :data)
+                                       :add-todo "/add-todo?todo=%s"}}} :data)
                 generate-string))
           (ring.util.response/content-type "text/json")))
     login-prompt))
@@ -116,22 +115,21 @@
 (defn complete-todo
   "sends assert request
   marking todo as complete
-  and then redirects"
+  congratulations!"
   [{route :uri params :params
     :as request}]
   (if-let [user (-> request :cookies (get "user") :value)]
     (let [id (get params "id")]
       (todos/complete-todo id)
-      (-> {:data ""
-           :meta {:docstring ""
+      (-> {:data "task asserted as complete? = true"
+           :meta {:docstring (str "includes task id pre-formatted into :incomplete-todo to assert task as complete? = false, and :forget-todo to retract task. " links-guide)
                   :links {:logout "/logout"
-                          :home "/"
-                          :incomplete-todo ""
-                          :forget-todo ""
+                          :incomplete-todo (str "/incomplete-todo?id=" id)
+                          :forget-todo (str "/forget-todo?id=" id)
                           :task-chart "/chart"
                           :task-graph "/graph"
-                          :get-incomplete-todos ""
-                          :get-complete-todos ""}}}
+                          :get-todos "/todos"
+                          :get-dones "/dones"}}}
           generate-string
           ring.util.response/response
           (ring.util.response/content-type "text/json")))
@@ -140,22 +138,21 @@
 (defn incomplete-todo
   "sends assert request
   marking task as incomplete
-  and then redirects"
+  better luck next time"
   [{route :uri params :params
     :as request}]
   (if-let [user (-> request :cookies (get "user") :value)]
     (let [id (get params "id")]
       (todos/incomplete-todo id)
-      (-> {:data ""
-           :meta {:docstring ""
+      (-> {:data "task asserted as complete? = false"
+           :meta {:docstring (str "includes task id pre-formatted into :complete-todo to assert task as complete? = true, and :forget-todo to retract task. " links-guide)
                   :links {:logout "/logout"
-                          :home "/"
-                          :complete-todo ""
-                          :forget-todo ""
+                          :complete-todo (str "/complete-todo?id=" id)
+                          :forget-todo (str "/forget-todo?id=" id)
                           :task-chart "/chart"
                           :task-graph "/graph"
-                          :get-incomplete-todos ""
-                          :get-complete-todos ""}}}
+                          :get-todos "/todos"
+                          :get-dones "/dones"}}}
           generate-string
           ring.util.response/response
           (ring.util.response/content-type "text/json")))
@@ -164,7 +161,7 @@
 (defn forget-todo
   "sends retract request
   removing task completely
-  and then redirects"
+  don't do this often."
   [{route :uri params :params
     :as request}]
   (if-let [user (-> request :cookies (get "user") :value)]
@@ -172,14 +169,14 @@
       (-> id
           todos/get-todo ffirst
           todos/remove-todo )
-      (-> {:data ""
-           :meta {:docstring ""
+      (-> {:data "task retracted from history"
+           :meta {:docstring links-guide
                   :links {:logout "/logout"
-                          :home "/"
                           :task-chart "/chart"
                           :task-graph "/graph"
-                          :get-incomplete-todos ""
-                          :get-complete-todos ""}}}
+                          :add-todo "/add-todo?todo=%s"
+                          :get-todos "/todos"
+                          :get-dones "/dones"}}}
           generate-string
           ring.util.response/response
           (ring.util.response/content-type "text/json")))
@@ -193,14 +190,13 @@
   (if-let [user (-> request :cookies (get "user") :value)]
     (let [description (get params "todo")]
       (todos/add-new-todo user description)
-      (-> {:data "successfully added new todo"
-           :meta {:docstring ""
+      (-> {:data (str "successfully added new todo : " description)
+           :meta {:docstring links-guide
                   :links {:logout "/logout"
-                          :home "/"
                           :task-chart "/chart"
                           :task-graph "/graph"
-                          :get-incomplete-todos ""
-                          :get-complete-todos ""}}}
+                          :get-todos "/todos"
+                          :get-dones "/dones"}}}
           generate-string
           ring.util.response/response
           (ring.util.response/content-type "text/json")))
@@ -208,7 +204,7 @@
 
 (defn todo-chart
   "gets task history
-  and formats it to table
+  and returns nested vectors
   in cheshire json"
   [{route :uri params :params
     :as request}]
@@ -218,8 +214,9 @@
           {:data (todos/tx-graph-for-user user)
            :meta {:docstring "vector of [timestamp task-count] vectors"
                   :links {:logout "/logout"
-                          :home "/"
-                          :add-todo "/add-todo?todo={description}"}}})
+                          :get-complete-todos "/dones"
+                          :get-todos "/todos"
+                          :add-todo "/add-todo?todo=%s"}}})
          (ring.util.response/content-type "text/json")))
     login-prompt))
 
